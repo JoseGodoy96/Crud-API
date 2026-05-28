abf1eb2e40635ea11957cb310e65d736a5f6bc76
Jose Maria <josemariagodoy1996@gmail.com>
2026-05-28 17:25:19 +0100

    <feat>(TaskService.java): Finalizacion de la entidad TaskService se encarga de realizar las rutas GET, POST, DELETE, UPDATE

--- crud-api/src/main/java/com/chema/db/crudapi/service/TaskService.java
+++ crud-api/src/main/java/com/chema/db/crudapi/service/TaskService.java
@@ -1,4 +1,48 @@
 package com.chema.db.crudapi.service;
 
+import com.chema.db.crudapi.model.Task;
+import com.chema.db.crudapi.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+// Esta clase es un service Beam
+@Service
+
 public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Long id, Task updatedTask) {
+        return taskRepository.findById(id)
+                .map(task -> {
+                    task.setTitle(updatedTask.getTitle());
+                    task.setDescription(updatedTask.getDescription());
+                    task.setStatus(updatedTask.getStatus());
+
+                    return taskRepository.save(task);
+                })
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
 }
