7eb683f65cb41af85e241beee1a215048cc61786
Jose Maria <josemariagodoy1996@gmail.com>
2026-05-28 17:12:12 +0100

    <feat>(TaskRepository.java): Fin de la clase TaskRepository

--- crud-api/src/main/java/com/chema/db/crudapi/repository/TaskRepository.java
+++ crud-api/src/main/java/com/chema/db/crudapi/repository/TaskRepository.java
@@ -1,4 +1,21 @@
 package com.chema.db.crudapi.repository;
 
-public class TaskRepository {
+// Llama a la base de datos
+import com.chema.db.crudapi.model.Task;
+
+/*
+Es una interfaz genérica que Spring ya implementa automáticamente.
+Te da:
+
+save
+findById
+findAll
+delete
+count
+existsById
+*/
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
