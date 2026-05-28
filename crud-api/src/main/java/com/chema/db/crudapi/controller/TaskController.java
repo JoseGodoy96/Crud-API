929853502f61355d0614bd7549d47ba029e2c892
Jose Maria <josemariagodoy1996@gmail.com>
2026-05-28 17:36:20 +0100

    <feat>(TaskController.java): Entidad de la entidad TaskController

--- crud-api/src/main/java/com/chema/db/crudapi/controller/TaskController.java
+++ crud-api/src/main/java/com/chema/db/crudapi/controller/TaskController.java
@@ -1,5 +1,63 @@
 package com.chema.db.crudapi.controller;
 
+import com.chema.db.crudapi.model.Task;
+import com.chema.db.crudapi.service.TaskService;
 
+/*
+Importa anotaciones REST de Spring.
+
+Acá vienen:
+
+@GetMapping
+@PostMapping
+@PutMapping
+@DeleteMapping
+@RequestBody
+@PathVariable
+
+etc.
+ */
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+// Indica a Spring que esta clase maneja EndPoints combina @Controller y @ResponseBody
+@RestController
+// Define la URL del Controller
+@RequestMapping("/api/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Task> getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id,
+                           @RequestBody Task task) {
+
+        return taskService.updateTask(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
 }
