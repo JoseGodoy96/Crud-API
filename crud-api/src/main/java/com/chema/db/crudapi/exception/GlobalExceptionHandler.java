3eba89e0163d240b0634d522df2432e6c5c9d491
Jose Maria <josemariagodoy1996@gmail.com>
2026-05-28 20:13:05 +0100

    <feat>(GlobalExceptionHandler.java): Creamos el ResponseEntity

--- crud-api/src/main/java/com/chema/db/crudapi/exception/GlobalExceptionHandler.java
+++ crud-api/src/main/java/com/chema/db/crudapi/exception/GlobalExceptionHandler.java
@@ -8,6 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import java.util.HashMap;
+
 import java.time.LocalDateTime;
 import java.util.Map;
 
@@ -26,5 +29,18 @@ public class GlobalExceptionHandler {
         );
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return ResponseEntity.badRequest().body(errors);
     }
 }
