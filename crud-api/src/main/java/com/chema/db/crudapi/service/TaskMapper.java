dce3d8e2d77a35707f5059ae719378cac303518b
Jose Maria <josemariagodoy1996@gmail.com>
2026-05-29 20:39:03 +0100

    <feat>(Taskmapper.java): Finalizacion de la clase TaskMapper

--- crud-api/src/main/java/com/chema/db/crudapi/service/TaskMapper.java
+++ crud-api/src/main/java/com/chema/db/crudapi/service/TaskMapper.java
@@ -1,4 +1,31 @@
 package com.chema.db.crudapi.service;
 
+import com.chema.db.crudapi.dto.TaskRequest;
+import com.chema.db.crudapi.dto.TaskResponse;
+import com.chema.db.crudapi.model.Task;
+
 public class TaskMapper {
+
+    public static Task toEntity(TaskRequest request) {
+        Task task = new Task();
+
+        task.setTitle(request.getTitle());
+        task.setDescription(request.getDescription());
+        task.setStatus(request.getStatus());
+
+        return task;
+    }
+
+    public static TaskResponse toResponse(Task task) {
+        TaskResponse response = new TaskResponse();
+
+        response.setId(task.getId());
+        response.setTitle(task.getTitle());
+        response.setDescription(task.getDescription());
+        response.setStatus(task.getStatus());
+        response.setCreatedAt(task.getCreatedAt());
+        response.setUpdatedAt(task.getUpdatedAt());
+
+        return response;
+    }
 }
