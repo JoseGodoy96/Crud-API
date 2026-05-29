c6cf211c37c7c7c3c2385fc56bb12b3eb044238c
Jose Maria <josemariagodoy1996@gmail.com>
2026-05-29 20:12:41 +0100

    <feat>(TaskService.java): Finalizacion de la clase TaskRequest

--- crud-api/src/main/java/com/chema/db/crudapi/dto/TaskRequest.java
+++ crud-api/src/main/java/com/chema/db/crudapi/dto/TaskRequest.java
@@ -1,4 +1,41 @@
 package com.chema.db.crudapi.dto;
 
+import com.chema.db.crudapi.model.TaskStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class TaskRequest {
+
+    @NotBlank(message = "Title is required")
+    @Size(max = 100)
+    private String title;
+
+    @Size(max = 255)
+    private String description;
+
+    private TaskStatus status;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
 }
