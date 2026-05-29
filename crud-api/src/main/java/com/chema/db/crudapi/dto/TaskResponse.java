35bfa97e87ae56184d06853e2aadd0b78d2c90c8
Jose Maria <josemariagodoy1996@gmail.com>
2026-05-29 20:34:27 +0100

    <feat>(TaskService.java): Finalizacion de TaskResponse

--- crud-api/src/main/java/com/chema/db/crudapi/dto/TaskResponse.java
+++ crud-api/src/main/java/com/chema/db/crudapi/dto/TaskResponse.java
@@ -1,4 +1,62 @@
 package com.chema.db.crudapi.dto;
 
+import com.chema.db.crudapi.model.TaskStatus;
+import java.time.LocalDateTime;
+
 public class TaskResponse {
+
+    private Long id;
+    private String title;
+    private String description;
+    private TaskStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
