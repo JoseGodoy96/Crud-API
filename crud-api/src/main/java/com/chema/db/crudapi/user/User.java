d2e438bf76daad1dff51ad0a2918b62192512137
Jose Maria <josemariagodoy1996@gmail.com>
2026-06-04 21:00:15 +0100

    <feat>(User.java): Finalizacion de la clase User.java

---
+++ crud-api/src/main/java/com/chema/db/crudapi/user/User.java
@@ -0,0 +1,37 @@
+package com.chema.db.crudapi.user;
+
+import jakarta.persistence.*;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+// Import para usar lombok
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 30, message = "Username must be between 3 and 30 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
