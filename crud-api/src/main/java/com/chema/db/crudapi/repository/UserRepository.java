4e75761bc3389d5ab12fdcb9e085be91511f2f2c
Jose Maria <josemariagodoy1996@gmail.com>
2026-06-04 21:08:18 +0100

    <feat>(UserRepository.java): Finalizacion de la clase UserRepository.java

---
+++ crud-api/src/main/java/com/chema/db/crudapi/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.chema.db.crudapi.repository;
+
+import com.chema.db.crudapi.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
