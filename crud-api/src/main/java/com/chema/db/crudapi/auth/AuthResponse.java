b929dc8afa72f38dabb3e48ba76f2e6fa9961976
Jose Maria <josemariagodoy1996@gmail.com>
2026-06-06 14:50:22 +0100

    <feat>(AuthResponse.java): Realizacion de la clase AuthResponse para la respuesta del token

---
+++ crud-api/src/main/java/com/chema/db/crudapi/auth/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.chema.db.crudapi.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+}
